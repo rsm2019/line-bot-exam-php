c3a07098018fd5e68e0d427dad98c429cc14e4ba
rsm2019 <46739531+rsm2019@users.noreply.github.com>
2020-05-09 14:22:12 +0700

    modified:   webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,8 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
-
+$access_token = 'U5skBY8gaVKoQFD+Fxs7IYBoTVwRSCerypoLb3vKgME53+f3zdiJtq4mbh73rABgfAp45jbdiyRYdBtI1gossvJ8ZG2X7yaFGVOWnCtkM2nlCyBIpKAIRVGphL/MauZCCMQ+nY0GgxwIE5smSGgelwdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
